e59f62b32deb04324b154bb8b6c3cbffb98f2790
prepucio <samueljosueariasrodriguez@gmail.com>
2026-02-27 17:29:39 -0500

    Actualizar Menú

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,14 +8,57 @@ public class Main {
 
         Mascota m = new Mascota("Luna", "Perro", 3, 8.5, true);
 
-        System.out.println(">> DATOS DE LUNA:");
+
+
+        System.out.println(">> Crear Mascota:");
         m.mostrarFicha();
 
         int opcion = 0;
 
-        while (opcion != 5) {
+        List<Mascota> lstNombre = new ArrayList<>();
+        int op;
+        do{
+            System.out.println("selecciona una opción");
+            op = sc.nextInt();
+            System.out.println("1. crear la mascota");
+            System.out.println("2. mostrar la lista");
+            System.out.println("3. salir");
 
-            System.out.println("\n====== MENÚ ======");
+            switch (op){
+                case 1:
+                    System.out.println("1, crear la mascota");
+                    String Mascota = sc.next();
+
+                    System.out.println("ingrese la especie de la mascota");
+                    String especie = sc.next();
+
+
+                    System.out.println("ingrese el nombre de la mascota");
+                    String nombre = sc.next();
+
+                    System.out.println("ingrese el peso de la mascota");
+                    Double peso = sc.nextDouble();
+
+                    System.out.println("ingrese la edad de la mascota");
+                    int edad = sc.nextInt();
+                    break;
+
+                case 2:
+                    System.out.println("2. Mostrar la ficha");
+                    for (Mascota p: lstNombre){
+                        System.out.println(p);
+                    }
+                    break;
+                case 3:
+                    System.out.println("3. salir");
+
+
+            }
+        } while (op != 3);
+
+        do {
+
+            System.out.println("====== MENÚ ======");
             System.out.println("1. Modificar edad");
             System.out.println("2. Modificar peso");
             System.out.println("3. Enfermar");
@@ -25,7 +70,7 @@ public class Main {
 
             switch (opcion) {
                 case 1:
-                    System.out.println("\n>> ANTES:");
+                    System.out.println(">> ANTES:");
                     m.mostrarFicha();
                     System.out.print("¿Cuántos años quieres sumar? (negativo para restar): ");
                     int anios = sc.nextInt();
@@ -36,12 +81,12 @@ public class Main {
                     } else if (anios < 0) {
                         m.setEdad(m.getEdad() + anios);
                     }
-                    System.out.println("\n>> DESPUÉS:");
+                    System.out.println(">> DESPUÉS:");
                     m.mostrarFicha();
                     break;
 
                 case 2:
-                    System.out.println("\n>> ANTES:");
+                    System.out.println(">> ANTES:");
                     m.mostrarFicha();
                     System.out.print("¿Cuántos kg quieres sumar? (negativo para restar): ");
                     double kilos = sc.nextDouble();
@@ -52,23 +97,23 @@ public class Main {
                     } else if (kilos < 0) {
                         m.setPeso(m.getPeso() + kilos);
                     }
-                    System.out.println("\n>> DESPUÉS:");
+                    System.out.println(">> DESPUÉS:");
                     m.mostrarFicha();
                     break;
 
                 case 3:
-                    System.out.println("\n>> ANTES:");
+                    System.out.println(">> ANTES:");
                     m.mostrarFicha();
                     m.enfermar();
-                    System.out.println("\n>> DESPUÉS:");
+                    System.out.println(">> DESPUÉS:");
                     m.mostrarFicha();
                     break;
 
                 case 4:
-                    System.out.println("\n>> ANTES:");
+                    System.out.println(">> ANTES:");
                     m.mostrarFicha();
                     m.recuperarSalud();
-                    System.out.println("\n>> DESPUÉS:");
+                    System.out.println(">> DESPUÉS:");
                     m.mostrarFicha();
                     break;
 
@@ -79,7 +124,7 @@ public class Main {
                 default:
                     System.out.println("Opción inválida");
             }
-        }
+        } while (opcion != 5);
 
         sc.close();
 
